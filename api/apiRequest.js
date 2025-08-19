bc1e5153c980484e2230de23a867e085b6a5f691
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2025-08-19 09:11:55 +0100

    implement apiPost function for handling POST requests with encoded data

--- api/apiRequest.js
+++ api/apiRequest.js
@@ -9,3 +9,19 @@ export const apiGet = async (url, headers = getAllHeader()) => {
   const response = await fetch(url, { headers });
   return response.json();
 };
+
+export const apiPost = async (url, data) => {
+  const encodedData = {
+    data: securedHeaderKeyPost + btoa(JSON.stringify(data)),
+  };
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(encodedData),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      secureddata: securedHeaderKeyPost + btoa(JSON.stringify(data)),
+    },
+  });
+  return response.json();
+};
