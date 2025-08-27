4af016570477f82a12f3ff47a06c17b94480b769
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2025-08-27 09:39:24 +0100

    Refactor apiPost and apiPut functions to use axios for consistency and remove commented-out code

--- api/apiRequest.js
+++ api/apiRequest.js
@@ -8,7 +8,7 @@ const api = axios.create({
 // Encode payload consistently
 const encodeData = (data) => ({
   data: securedHeaderKeyPost + btoa(JSON.stringify(data)),
-});
+}); 
 
 // 🔹 GET
 export const apiGet = async (url, headers = { secureddata: securedHeaderKey }) => {
@@ -16,21 +16,37 @@ export const apiGet = async (url, headers = { secureddata: securedHeaderKey }) =
   return res.data;
 };
 
+// 🔹 POST
 export const apiPost = async (url, data) => {
-  const encodedData = {
-    data: securedHeaderKeyPost + btoa(JSON.stringify(data)),
-  };
-  const response = await fetch(url, {
-    method: "POST",
-    body: JSON.stringify(encodedData),
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      secureddata: securedHeaderKeyPost + btoa(JSON.stringify(data)),
-    },
+  const res = await api.post(url, encodeData(data), {
+    headers: { secureddata: securedHeaderKeyPost + btoa(JSON.stringify(data)) },
   });
-  return response.json();
+  return res.data;
 };
+
+// 🔹 PUT
+export const apiPut = async (url, data) => {
+  const res = await api.put(url, encodeData(data), {
+    headers: { secureddata: securedHeaderKeyPost + btoa(JSON.stringify(data)) },
+  });
+  return res.data;
+};
+
+// export const apiPost = async (url, data) => {
+//   const encodedData = {
+//     data: securedHeaderKeyPost + btoa(JSON.stringify(data)),
+//   };
+//   const response = await fetch(url, {
+//     method: "POST",
+//     body: JSON.stringify(encodedData),
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json",
+//       secureddata: securedHeaderKeyPost + btoa(JSON.stringify(data)),
+//     },
+//   });
+//   return response.json();
+// };
 
 export const apiDelete = async (url, data) => apiPost(url, data);
 export const apiUpdate = async (url, data) => apiPost(url, data);
