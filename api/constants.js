cbba28cef50313765a1070030c40597334191ac8
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2025-09-10 11:01:00 +0100

    updated

--- api/constants.js
+++ api/constants.js
@@ -1,6 +1,5 @@
-// Pick up base API URL from environment, with a fallback (local dev)
-export const BASE_URL = import.meta.env.process.env.API_BASE_URL || "http://localhost:8080/api";
+export const BASE_URL = import.meta.env.process.env.API_BASE_URL || "http://localhost:8073api";
 
 // Secure keys (can also be loaded from env for security)
-export const securedHeaderKey = import.meta.env.process.env.API_SECURED_KEY_GET || "getall";
-export const securedHeaderKeyPost = import.meta.env.process.env.API_SECURED_KEY_POST || "hguwgtyae4z";
+export const securedHeaderKey = import.meta.env.process.env.API_SECURED_KEY_GET || "getpast";
+export const securedHeaderKeyPost = import.meta.env.process.env.API_SECURED_KEY_POST || "hguwgtyae2l";
