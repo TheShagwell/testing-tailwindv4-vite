15ccdef7b9aaabe413dc3fc852ba33336a27de42
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2025-12-14 23:44:56 +0100

    ...

--- src/userSetup.jsx
+++ src/userSetup.jsx
@@ -29,7 +29,7 @@ export default function UserProfile() {
             <label className="text-sm font-medium">Job</label>
             <Input type="text" value={user.job} readOnly className="cursor-not-allowed" />
           </div>
-          <Button>Edit Profile Detail</Button>
+          <Button className="mt-4">Edit Profile Detail</Button>
         </CardContent>
       </Card>
     </div>
